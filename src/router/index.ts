ac946f102fe3cfe0f25390b1f2b26c7c4d299562
hdq <2414154707@qq.com>
2023-06-29 21:00:22 +0800

    登入页面搭建及路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,14 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/user/login'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/user/login',
+    name: 'UserLogin',
+    component: () => import(/* webpackChunkName: "UserLogin" */ '../views/user/login.vue')
   }
 ]
 
