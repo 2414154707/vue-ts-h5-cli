a20b8fa21f6456f4e2b8a1ccf9211b41b20ea2be
hdq <2414154707@qq.com>
2023-06-29 15:02:05 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-});
+  devServer: {
+    host: 'localhost',
+    port: 8088,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7001',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+})
